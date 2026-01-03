f59ed3d6918907919b3b559fff9de1b61502f717
Yuhang (Tom) LIN <yhlin@iastate.edu>
2026-01-02 18:47:54 -0600

    Update 职位置顶（改）.js

--- 职位置顶（改）/职位置顶（改）.js
+++ 职位置顶（改）/职位置顶（改）.js
@@ -14,7 +14,7 @@ const map = new Map()
         map.set(job, $li)
     })
 
-let job_l = ['中文名', '别名', '动画制作', '导演', '系列构成', '脚本', '制作管理', '人物设定', '总作画监督', '人物原案', '原作', '原案', '放送开始', '播放结束', '上映年度', '话数', '片长']
+let job_l = ['中文名', '别名', '动画制作', '导演', '系列构成', '脚本', '制作管理', '人物设定', '总作画监督', '人物原案', '原作', '原案', '放送开始', '播放结束', '上映年度', '话数', '片长', '在线播放平台']
 if (chiiApp.cloud_settings.get('job_l')) {
     job_l = JSON.parse(chiiApp.cloud_settings.get('job_l'))
 } else if (localStorage.getItem('4941')) {
