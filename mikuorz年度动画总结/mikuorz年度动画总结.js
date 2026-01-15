e8616e337705cfc48769ebfb515229973e04cd99
Yuhang (Tom) LIN <yhlin@iastate.edu>
2026-01-15 12:07:53 -0600

    mikuorz年度动画总结 / v1.0.0

---
+++ mikuorz年度动画总结/mikuorz年度动画总结.js
@@ -0,0 +1,11 @@
+let isUserPage = window.location.href.match(/\/user\/(.*)$/i);
+if (isUserPage) {
+    let username = isUserPage[1];
+
+    $('.network_service').append(
+        '<li>' +
+        '<span class="service" style="background-color:#39c5bb;">Mikuorz</span>' +
+        `<a rel="me" class="l" href="https://report.mikuorz.top/report.html?uid=${username}" target="_blank">生成年度动画总结</a>` +
+        '</li>'
+    );
+}
